091bf6ed6009e23ad03d0f13a6b11dcdf96234a2
babygirl-hub <anokwuruchinomso@gmail.com>
2025-08-23 10:38:51 +0100

    third commit

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" { 
   region = "us-east-1"
 }
-resource "aws_s3_bucket" "test_buckets" {
-  bucket = "queen_artifact_buckets"
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "queen_artifact_bucket"
   tags = {
-   Name = "queen_test_buckets"
+   Name = "queen_test_bucket"
    Environment = "DEV"
   }
 }
